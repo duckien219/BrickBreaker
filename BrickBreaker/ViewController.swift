a468fada77d1aca3f51c20414ce14aa61ebfc4c5
Kien Nguyen Duc <duckien219@gmail.com>
2017-06-14 10:23:06 +0700

    Initial Commit

---
+++ BrickBreaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrickBreaker
+//
+//  Created by Kien Nguyen Duc on 6/14/17.
+//  Copyright © 2017 Kien Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
